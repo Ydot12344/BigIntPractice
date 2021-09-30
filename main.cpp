aea26631dbe146162a826365b1b8b85cbc32642b
Andrew Zdor <amzdor@edu.hse.ru>
2021-09-30 20:34:10 +0300

    Advanced operator searching

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 #include "BigInt.h"
+#include <unordered_map>
+
+std::unordered_map<std::string, BigInt (BigInt::*) (const BigInt&) const> arithmetic_op = {
+        {"+", &BigInt::operator+}, {"-", &BigInt::operator-}, {"*", &BigInt::operator*},
+        {"/", &BigInt::operator/}, {"%", &BigInt::operator%}
+};
+std::unordered_map<std::string, bool (BigInt::*) (const BigInt&) const> logic_op = {
+        {">", &BigInt::operator>}, {">=", &BigInt::operator>=},{"<", &BigInt::operator<},
+        {"<=", &BigInt::operator<=}, {"==", &BigInt::operator==}, {"!=", &BigInt::operator!=}
+};
 
 int main() {
     BigInt a;
@@ -7,27 +17,9 @@ int main() {
     std::string op;
     std::cin >> a  >> op >> b;
 
-    if(op == "+")
-        std::cout << a + b;
-    else if(op == "-")
-        std::cout << a - b;
-    else if(op == "*")
-        std::cout << a * b;
-    else if(op == "/")
-        std::cout << a / b;
-    else if(op == "%")
-        std::cout << a % b;
-    else if(op == ">")
-        std::cout << (a > b);
-    else if(op == "<")
-        std::cout << (a < b);
-    else if(op == ">=")
-        std::cout << (a >= b);
-    else if(op == "<=")
-        std::cout << (a <= b);
-    else if(op == "==")
-        std::cout << (a == b);
-    else if(op == "!=")
-        std::cout << (a != b);
+    if(arithmetic_op.find(op) != arithmetic_op.end())
+        std::cout << (&a->*(arithmetic_op[op]))(b);
+    else if(logic_op.find(op) != logic_op.end())
+        std:: cout << (&a->*(logic_op[op]))(b);
     return 0;
 }
